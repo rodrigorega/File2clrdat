363264438462c89f6561ae7fa8dec754db1b70b6
Rodrigo Rega <contacto@rodrigorega.es>
2014-09-23 23:38:59 +0200

    Added search in dat file
    Some minor changes too:
    - Added docopt to parse command line args.
    - Updated README.
    - Updated TODO.

--- file2clrdat.py
+++ file2clrdat.py
@@ -2,19 +2,28 @@
 # -*- coding: utf-8 -*-
 
 """
-This Python script generates "line rom" info from a file or files in a dir.
-This info can be added manually to a ClrMamePro .dat.
+file2clrdat.py: Outputs a file in ClrMamePro dat format with rom info (hashes,
+size...) from a file or files in a directory. This will speed up the process
+of update ClrMamePro .dat files, without using ClrMamePro.
 
-Generates one file with rom data from a sngle file. This data can be
-merged in a existing ClrMamePro .dat file.
-I use this to speed up the process of update my .dat files, without using
-ClrMamePro.
+Usage:
+  file2clrdat.py INPUT_ROM [-d DAT_FILE]
+  file2clrdat.py --help
+
+Arguments:
+  INPUT_ROM                           Input rom file o directory with rom files
+
+Options:
+  -d DAT_FILE, --datfile=DAT_FILE     Search INPUT_ROM md5 hash in a
+                                      ClrMamePro dat file and notify on match
+  -h, --help                          Show this help screen.
+
+Output:
+  INPUT_ROM_romdata, File with all info from INPUT_ROM
+  
 
 Author: Rodrigo Rega <contacto@rodrigorega.es>
 License: CC-BY-SA 3.0 license (http://creativecommons.org/licenses/by/3.0/
-
-Use: file2clrdat filename.rom
-Output: filename.rom_romdata
 """
 
 from __future__ import print_function  # used for print to files
@@ -23,13 +32,16 @@ import string  # needed for templating
 import os  # needed for file and path manipulations
 from file import File  # class for get file data (hashes, size, etc)
 
+from lxml import etree, objectify # for parsinge dat file
+import urllib
+import pprint
 
 class File2clrdat:
     """
     file2clrdat class
     """
 
-    def __init__(self, inputPath):
+    def __init__(self, inputPath, datFilePath):
         """
         Class initialiser
         
@@ -37,10 +49,15 @@ class File2clrdat:
         
         :type inputPath: string
         :param inputPath: file or directory to work with
+        
+        :type datFilePath: string
+        :param datFilePath: work dat file
         """
         self.inputPath = inputPath
+        self.datFilePath = datFilePath
 
     inputPath = None
+    datFilePath = None
     fileData = None
     SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
     romTemplateFile = os.path.join(SCRIPT_PATH, 'ClrMamePro_rom_dat.tpl')
@@ -60,6 +77,23 @@ class File2clrdat:
             self.__processDirectory()
         else:
             print("Invalid file or directory")
+            
+    def searchInDatFile(self, search_type, search_content):
+        """
+        Checks if a given content is insde dat file
+
+        Return: If content was found, returns filename
+        """
+        with open(self.datFilePath) as f_xml:
+            xml_data = f_xml.read()
+
+        datafile = objectify.fromstring(xml_data)
+        
+        # loop over elements and print their tags and text
+        for games in datafile.getchildren():
+            for game in games.getchildren():
+                if game.get(search_type) == search_content:
+                    return game.get("name")
 
     def __processFile(self):
         """
@@ -69,10 +103,45 @@ class File2clrdat:
         """
         self.fileData = File(self.inputPath)
         self.fileData.getHashes()
+        
+        if self.datFilePath:
+            romFoundInDatFile = self.searchInDatFile('md5', self.fileData.md5)
+            self.__printMatchFoundInDatFile(self.inputPath, romFoundInDatFile)
+            confirm = self.__userConfirm('Do you want to generate romdata file anyway? (y or n)')
+            if confirm:
+                self.__getTemplateContent()
+                self.__populateTemplate()
+                self.__writePopulatedTemplate()
+            else:
+                print('Exiting...')
+                return
+        else:
+            # TODO: I dont like this, it is duplicated with previous if...
+            self.__getTemplateContent()
+            self.__populateTemplate()
+            self.__writePopulatedTemplate()
+    
+    def __userConfirm(self, confirmMsg):
+        """get user confirmation to proceed"""
+        userChoice = raw_input(confirmMsg)        
+        if userChoice == 'y':
+            return True
+        elif userChoice == 'n':
+            return False
+        else:
+            return self.__userConfirm(confirmMsg)
+        
+    def __printMatchFoundInDatFile(self, inputRomFile, datFile):
+        """ Print msg fom found """
+        
+        message = """
+        Input rom file found in provided dat file:
+        - Input rom file: %s
+        - Rom name in dat: %s
+        """ % (inputRomFile, datFile)
 
-        self.__getTemplateContent()
-        self.__populateTemplate()
-        self.__writePopulatedTemplate()
+        print(message)
+
 
     def __processDirectory(self):
         """
@@ -137,8 +206,18 @@ if __name__ == "__main__":
         print('You must use Python 2,7 or higher')
         sys.exit(2)
 
-    if len(sys.argv) != 2:
-        print("Use: python file2clrdat.py FILE_OR_DIRECTORY")
-    else:
-        file2clrdat = File2clrdat(sys.argv[1])
-        file2clrdat.generateRomData()
+    try:
+        from docopt import docopt
+    except:
+        print ("""
+        Please install docopt using:
+            pip install docopt
+        For more refer to:
+        https://github.com/docopt/docopt
+        """)
+        raise
+        
+    args = docopt(__doc__, version='1.0.0rc2')
+    
+    file2clrdat = File2clrdat(args['INPUT_ROM'],args['--datfile'])
+    file2clrdat.generateRomData()
